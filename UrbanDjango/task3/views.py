adc617214204ab9dd22e34c0b80393abb0a559d6
Yed Makarov <myed@list.ru>
2025-01-14 02:01:29 +0500

    Базовые HTML тэги в шаблонах.

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def func_template(request):
+    return render(request, 'second_task/func_template.html')
+
+
+class class_template(TemplateView):
+    template_name = 'second_task/class_template.html'
