5eea9957d97563cd3ad35770f3447fdf1d31c910
Yed Makarov <myed@list.ru>
2025-01-15 00:34:57 +0500

    Базовые HTML тэги в шаблонах.

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -11,8 +11,18 @@ def platform(request):
     return render(request, 'thrid_task/platform.html')
 
 
+# def games(request):
+#     return render(request, 'thrid_task/games.html')
 def games(request):
-    return render(request, 'thrid_task/games.html')
+    game1 = 'Atomic Heart '
+    game2 = 'Cyberpunr 2077 '
+    game3 = 'PayDay 2 '
+    context = {
+        'game1': game1,
+        'game2': game2,
+        'game3': game3
+    }
+    return render(request, 'thrid_task/games.html', context)
 
 
 def cart(request):
